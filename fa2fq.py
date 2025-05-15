fe1878058cf7dda4786b38fed59ced216f4c22e6
Simon Hegele <103111576+SimonHegele@users.noreply.github.com>
2025-05-15 15:00:42 +0200

    Add files via upload

--- fa2fq.py
+++ fa2fq.py
@@ -1,6 +1,8 @@
 from argparse   import ArgumentParser
+from logging    import error, info
 from numpy      import log10
 from os         import path
+from typing     import Generator
 
 from file_services.fasta_file_service import FastaFileService
 from file_services.fastq_file_service import FastqFileService
@@ -29,30 +31,27 @@ class MyArgumentParser(ArgumentParser):
                           type=int,
                           default=33,
                           help="Phred-quality offset (Default: 33)")
-        self.add_argument("-v","--verbose",
-                          help="Show conversion progress",
-                          action='store_true')
         
     def check_params(self)->None:
 
         # Checking file paths
         if not path.isfile(self.args.FASTA):
-            print("File FASTA does not exist")
+            error("File FASTA does not exist")
             exit(1)
         if path.isfile(".".join(self.args.FASTA.split(".")[:-1])+".fastq"):
-            outfile = ".".join(self.args.FASTA.split(".")[:-1])+".fastq"
-            print(f"{outfile} exists")
+            f_out = ".".join(self.args.FASTA.split(".")[:-1])+".fastq"
+            error(f"{f_out} exists")
             exit(1)
 
         # Checking error values
         if not ((0 < self.args.S) and (self.args.S < 1)):
-            print("S must be greater than 0 and smaller than 1")
+            error("S must be greater than 0 and smaller than 1")
             exit(1)
         if not ((0 < self.args.s) and (self.args.s < 1)):
-            print("s must be greater than 0 and smaller than 1")
+            error("s must be greater than 0 and smaller than 1")
             exit(1)
         if self.args.S > self.args.s:
-            print("S < s (Upper case character represent higher quality bases, therefore S should be smaller than s)")
+            error("S < s (Upper case character represent higher quality bases, therefore S should be smaller than s)")
             exit(1)
 
     def parse_args(self):
@@ -66,12 +65,18 @@ def phred(error_rate: float, offset: int):
 
     return chr(int(-10*log10(error_rate))+offset)
 
-def fasta_2_fastq(read: dict[str, str], P: chr, p: chr) -> None:
+def fasta_2_fastq(sequences: list[dict[str, str]], P: chr, p: chr) -> Generator:
 
-    read["file_type"]   = "fastq"
-    read["info"]        = "+"
-    read["quality"]     = "".join([P if b.isupper() else p for b in read["sequence"]])
-    read["sequence"]    = read["sequence"].upper()
+    for i, sequence in enumerate(sequences):
+        if i % 100_000 == 0:
+            print(f"{i:>10}")
+
+        sequence["file_type"]   = "fastq"
+        sequence["info"]        = "+"
+        sequence["quality"]     = "".join([P if b.isupper() else p for b in sequence["sequence"]])
+        sequence["sequence"]    = sequence["sequence"].upper()
+
+        yield sequence
         
 def main():
 
@@ -80,24 +85,15 @@ def main():
     P = phred(args.S, args.offset)
     p = phred(args.s, args.offset)
 
-    print("Reading...")
-    reads = list(FastaFileService().read(args.FASTA))
-    print("Done")
+    reader = FastaFileService()
+    writer = FastqFileService()
+    f_out  = ".".join(args.FASTA.split(".")[:-1])+".fastq"
 
-    print("Conversion ...")
-    for i, read in reads:
-        if i % 100_000 == 0 and args.verbose:
-            print(f"Progress: {i} of {len(reads)}")
-        fasta_2_fastq(read, P, p) 
-    print("Done")
+    writer.write(f_out, fasta_2_fastq(reader.read(args.FASTA), P, p))
 
-    print("Writing...")
-    FastqFileService.write(".".join(args.FASTA.split(".")[:-1])+".fastq", reads)
-    print("Done")
-
-    print("##############################################")
-    print("#    Simon says: Thanks for using SSfSBT!    #")
-    print("##############################################")
+    info("##############################################")
+    info("#    Simon says: Thanks for using SSfSBT!    #")
+    info("##############################################")
 
 if __name__ == "__main__":
     main()
