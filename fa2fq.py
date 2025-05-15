dcef7066cc55e430d595c5b45d0fdc33f64a5cee
Simon Hegele <103111576+SimonHegele@users.noreply.github.com>
2025-05-15 14:38:53 +0200

    Update fa2fq.py

--- fa2fq.py
+++ fa2fq.py
@@ -1,5 +1,4 @@
 from argparse   import ArgumentParser
-from logging    import error, info
 from numpy      import log10
 from os         import path
 
@@ -38,22 +37,22 @@ class MyArgumentParser(ArgumentParser):
 
         # Checking file paths
         if not path.isfile(self.args.FASTA):
-            error("File FASTA does not exist")
+            print("File FASTA does not exist")
             exit(1)
         if path.isfile(".".join(self.args.FASTA.split(".")[:-1])+".fastq"):
             outfile = ".".join(self.args.FASTA.split(".")[:-1])+".fastq"
-            error(f"{outfile} exists")
+            print(f"{outfile} exists")
             exit(1)
 
         # Checking error values
         if not ((0 < self.args.S) and (self.args.S < 1)):
-            error("S must be greater than 0 and smaller than 1")
+            print("S must be greater than 0 and smaller than 1")
             exit(1)
         if not ((0 < self.args.s) and (self.args.s < 1)):
-            error("s must be greater than 0 and smaller than 1")
+            print("s must be greater than 0 and smaller than 1")
             exit(1)
         if self.args.S > self.args.s:
-            error("S < s (Upper case character represent higher quality bases, therefore S should be smaller than s)")
+            print("S < s (Upper case character represent higher quality bases, therefore S should be smaller than s)")
             exit(1)
 
     def parse_args(self):
@@ -81,24 +80,24 @@ def main():
     P = phred(args.S, args.offset)
     p = phred(args.s, args.offset)
 
-    info("Reading...")
-    reads = list(FastaFileService().read(args.LoRDEC_fasta))
-    info("Done")
+    print("Reading...")
+    reads = list(FastaFileService().read(args.FASTA))
+    print("Done")
 
-    info("Conversion ...")
+    print("Conversion ...")
     for i, read in reads:
         if i % 100_000 == 0 and args.verbose:
-            info(f"Progress: {i} of {len(reads)}")
+            print(f"Progress: {i} of {len(reads)}")
         fasta_2_fastq(read, P, p) 
-    info("Done")
+    print("Done")
 
-    info("Writing...")
+    print("Writing...")
     FastqFileService.write(".".join(args.FASTA.split(".")[:-1])+".fastq", reads)
-    info("Done")
+    print("Done")
 
-    info("##############################################")
-    info("#    Simon says: Thanks for using SSfSBT!    #")
-    info("##############################################")
+    print("##############################################")
+    print("#    Simon says: Thanks for using SSfSBT!    #")
+    print("##############################################")
 
 if __name__ == "__main__":
     main()
