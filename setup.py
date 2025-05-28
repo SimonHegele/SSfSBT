17cc0134fdcf6a4105b4367a0fe272c26d8f3b25
Simon Hegele <103111576+SimonHegele@users.noreply.github.com>
2025-05-29 00:00:14 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
                 "lengths=sequence_lengths:main",
                 "kallisto2nanosim=kallisto2nanosim:main",
                 "busco_merge=busco_merge:main",
-                "unambigous_codes=unambigous_codes:main"
+                "unambiguous_codes=unambiguous_codes:main"
             ],
         },
 )
