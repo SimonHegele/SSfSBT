bd2de1f76a84d243b3a046d3acc9c4b4e28c4bf2
Simon Hegele <103111576+SimonHegele@users.noreply.github.com>
2025-11-04 10:50:50 +0100

    Fix header parsing in FastaFileService
    Excluded separator from header

--- file_services/fasta_file_service.py
+++ file_services/fasta_file_service.py
@@ -8,7 +8,7 @@ class FastaFileService(fasta_like_file_service.FastaLikeFileService):
     def parse_string(cls, string: str)->dict:
 
         lines    = string.split("\n")
-        header   = lines[0]
+        header   = lines[0][1:]
         sequence = "".join(lines[1:])
 
         return {"header": header,
@@ -19,4 +19,5 @@ class FastaFileService(fasta_like_file_service.FastaLikeFileService):
     @classmethod
     def parse_dict(cls, read: dict)->str:
         
+
         return read["header"]+"\n"+read["sequence"]+"\n"
